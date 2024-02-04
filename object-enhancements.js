093ea3cc4d33bdbf62b9fdf677a05125a34b3949
Domenick <152310637+ddddsfsgf@users.noreply.github.com>
2024-02-04 13:33:28 -0500

    Add files via upload

---
+++ object-enhancements.js
@@ -0,0 +1,32 @@
+//Same keys and values
+function createInstructor(firstName, lastName){
+    return {
+      firstName,
+      lastName
+    }
+}
+//Computed Property Names
+let favoriteNumber = 42;
+const instructor = {
+    firstName: "Colt", 
+    [favoriteNumber]: "That is my favorite!"
+}
+//Object Methods
+let theInstructor = {
+    firstName: "Colt",
+    sayHi(){
+        return "Hi!"
+    },
+    sayBye(){
+        return this.firstName + "says bye!"
+    }
+}
+//createAnimal function
+function createAnimal(species, verb, noise){
+    return {
+        species,
+        [verb](){
+            return noise;
+        }
+    }
+}
